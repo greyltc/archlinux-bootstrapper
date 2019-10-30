68d7b555f5cb0f30ba54604dd1e1eb82476a3561
Grey Christoforo <grey@christoforo.net>
2019-10-30 15:38:20 +0000

    trying fakechroot

--- build-wsl-root.sh
+++ build-wsl-root.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o verbose
+set -o xtrace
+
+#set -e -u -o pipefail
 
 wget -nv -e robots=off -r --no-parent -A 'archlinux-bootstrap-*' https://mirrors.edge.kernel.org/archlinux/iso/latest/
 mv mirrors.edge.kernel.org/archlinux/iso/latest/archlinux-bootstrap-* .
 rm -rf mirrors.edge.kernel.org
 
-gpg --no-default-keyring --keyring vendors.gpg --keyserver keyserver.ubuntu.com --recv-keys 4AA4767BBC9C4B1D18AE28B77F2D434B9741E8AC
-gpg --no-default-keyring --keyring vendors.gpg --list-keys --fingerprint --with-colons | sed -E -n -e 's/^fpr:::::::::([0-9A-F]+):$/\1:6:/p' | gpg --no-default-keyring --keyring vendors.gpg --import-ownertrust
-gpg --no-default-keyring --keyring vendors.gpg --verify *.sig
+gpg --no-default-keyring --keyring ./vendors.gpg --keyserver keyserver.ubuntu.com --recv-keys 4AA4767BBC9C4B1D18AE28B77F2D434B9741E8AC
+gpg --no-default-keyring --keyring ./vendors.gpg --list-keys --fingerprint --with-colons | sed -E -n -e 's/^fpr:::::::::([0-9A-F]+):$/\1:6:/p' | gpg --no-default-keyring --keyring ./vendors.gpg --import-ownertrust
+gpg --no-default-keyring --keyring ./vendors.gpg --verify *.sig
 
-rm *.sig vendors.gpg
+rm *.sig vendors.gpg*
 
 mv archlinux-bootstrap-* install.tar.gz
 
 mkdir rootmount
 archivemount install.tar.gz rootmount
 
-arch-chroot rootmount
-touch poop
+cat <<EOF > rootmount/root.x86_64/usr/bin/fs-setup.sh
+touch /poop
+EOF
 
-exit
+chmod +x rootmount/root.x86_64/usr/bin/fs-setup.sh
+
+fakechroot fakeroot arch-chroot rootmount/root.x86_64 fs-setup.sh
+
+#exit
 unmount rootmount
 
