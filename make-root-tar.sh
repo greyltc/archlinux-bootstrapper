e054e1588524f79ad4d38d1dc6b184878899fc1d
Travis CI <travis@rob.ot>
2019-10-31 17:06:36 +0000

    make root instead of downloading it

--- make-root-tar.sh
+++ make-root-tar.sh
@@ -6,35 +6,38 @@ set -o nounset
 set -o verbose
 set -o xtrace
 
-#set -e -u -o pipefail
+CURDIR=$(pwd)
+TMPDIR=$(mktemp -d /tmp/genbootstrap.XXXXXX)
 
-wget --continue -nv -e robots=off -r --no-parent -A 'archlinux-bootstrap-*' https://mirrors.edge.kernel.org/archlinux/iso/latest/
-mkdir -p /tmp/rootwork
-cp mirrors.edge.kernel.org/archlinux/iso/latest/archlinux-bootstrap-* /tmp/rootwork/.
-pushd /tmp/rootwork
+cat > "${TMPDIR}"/pacman.conf << "EOF"
+[options]
+Architecture = auto
+CheckSpace
+Color
+SigLevel = Required DatabaseOptional
+[core]
+Include = /etc/pacman.d/mirrorlist
+[extra]
+Include = /etc/pacman.d/mirrorlist
+[community]
+Include = /etc/pacman.d/mirrorlist
+EOF
 
-# verify .sig
-gpg --no-default-keyring --keyring ./vendors.gpg --keyserver keyserver.ubuntu.com --recv-keys 4AA4767BBC9C4B1D18AE28B77F2D434B9741E8AC
-gpg --no-default-keyring --keyring ./vendors.gpg --list-keys --fingerprint --with-colons | sed -E -n -e 's/^fpr:::::::::([0-9A-F]+):$/\1:6:/p' | gpg --no-default-keyring --keyring ./vendors.gpg --import-ownertrust
-gpg --no-default-keyring --keyring ./vendors.gpg --verify *.sig
-rm vendors.gpg* *.sig
+sudo mkdir "${TMPDIR}"/root
+sudo pacstrap -C "${TMPDIR}"/pacman.conf -c -d -G -M "${TMPDIR}"/root/ arch-install-scripts systemd
+sudo rm "${TMPDIR}"/root/var/lib/pacman/sync/*
 
-sudo tar xzf archlinux-bootstrap-*-x86_64.tar.gz
-
-cat <<EOF > /tmp/setup-tasks.sh
+cat > "${TMPDIR}"/setup-tasks.sh << "EOF"
 touch poop
 echo "poop touched!"
 EOF
-chmod +x /tmp/setup-tasks.sh
-sudo sh -c 'mv /tmp/setup-tasks.sh  /tmp/rootwork/root.x86_64/usr/bin/.'
+chmod +x "${TMPDIR}"/setup-tasks.sh
+sudo mv "${TMPDIR}"/setup-tasks.sh "${TMPDIR}"/root/usr/bin/setup-tasks.sh
 
-sudo /tmp/rootwork/root.x86_64/bin/arch-chroot /tmp/rootwork/root.x86_64/ setup-tasks.sh
-sudo rm /tmp/rootwork/root.x86_64/bin/setup-tasks.sh
+sudo "${TMPDIR}"/root/bin/arch-chroot "${TMPDIR}"/root/ setup-tasks.sh
+sudo rm "${TMPDIR}"/root/bin/setup-tasks.sh
 
-sudo sh -c 'cd /tmp/rootwork/root.x86_64 && tar -zcf ../root.tar.gz *'
+sudo sh -c "(cd ${TMPDIR}/root; bsdtar -cf - * | gzip -9 > ${TMPDIR}/root.tar.gz)"
+mv "${TMPDIR}/root.tar.gz" "${CURDIR}/root.tar.gz"
+sudo rm -rf "${TMPDIR}"
 
-popd
-
-mv /tmp/rootwork/root.tar.gz .
-
-sudo rm -rf /tmp/rootwork
