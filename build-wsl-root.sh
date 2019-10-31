3aedd8b22d5f8d7f79f5007ea8dbf4b63cc464b0
Grey Christoforo <grey@christoforo.net>
2019-10-31 13:11:21 +0000

    switch back to archivemount

--- build-wsl-root.sh
+++ build-wsl-root.sh
@@ -8,32 +8,31 @@ set -o xtrace
 
 #set -e -u -o pipefail
 
-wget -nv -e robots=off -r --no-parent -A 'archlinux-bootstrap-*' https://mirrors.edge.kernel.org/archlinux/iso/latest/
-mv mirrors.edge.kernel.org/archlinux/iso/latest/archlinux-bootstrap-* .
-rm -rf mirrors.edge.kernel.org
+wget --continue -nv -e robots=off -r --no-parent -A 'archlinux-bootstrap-*' https://mirrors.edge.kernel.org/archlinux/iso/latest/
+#ln -s mirrors.edge.kernel.org/archlinux/iso/latest/archlinux-bootstrap-* .
 
+# verify .sig
 gpg --no-default-keyring --keyring ./vendors.gpg --keyserver keyserver.ubuntu.com --recv-keys 4AA4767BBC9C4B1D18AE28B77F2D434B9741E8AC
 gpg --no-default-keyring --keyring ./vendors.gpg --list-keys --fingerprint --with-colons | sed -E -n -e 's/^fpr:::::::::([0-9A-F]+):$/\1:6:/p' | gpg --no-default-keyring --keyring ./vendors.gpg --import-ownertrust
 gpg --no-default-keyring --keyring ./vendors.gpg --verify *.sig
+rm vendors.gpg*
 
-rm *.sig vendors.gpg*
+#sudo tar xzf archlinux-bootstrap-*.tar.gz
+mkdir mnt
+sudo archivemount archlinux-bootstrap-*.tar.gz mnt
 
-mv archlinux-bootstrap-* install.tar.gz
-
-sudo tar xzf install.tar.gz
-rm install.tar.gz
+sudo sh -c 'mv mnt/root.x86_64/* mnt/.'
+sudo rm -rf mnt/root.x86_64
 
 cat <<EOF > /tmp/fs-setup.sh
-touch /poop
+touch poop
 echo "poop touched!"
 EOF
-chmod +x /tmp/fs-setup.sh
+chmod +x mnt/usr/bin/fs-setup.sh
+sudo mv  /tmp/fs-setup.sh  mnt/usr/bin/fs-setup.sh
 
-sudo mv /tmp/fs-setup.sh root.x86_64/usr/bin/.
+sudo arch-chroot mnt fs-setup.sh
+rm mnt/usr/bin/fs-setup.sh
 
-sudo arch-chroot root.x86_64 fs-setup.sh
-sudo rm root.x86_64/usr/bin/fs-setup.sh
-
-cd root.x86_64
-sudo tar czf ../install.tar.gz .
-exit
+sudo umount mnt
+rm -rf mnt
